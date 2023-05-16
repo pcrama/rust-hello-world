74c2001ff77788731a83990d50ed914cbd6de1aa
Philippe Crama <pcfeb0009@gmx.com>
2023-05-16 21:33:30 +0200

    Unable to compile on Raspberry Pi, try with other framework

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,15 @@
-use hyper::{Body, Request, Response, Server};
-use hyper::service::{make_service_fn, service_fn};
+use actix_web::{web, App, HttpResponse, HttpServer};
 
-async fn handle_request(_req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    let response = Response::new(Body::from("Hello, World!"));
-    Ok(response)
+async fn index() -> HttpResponse {
+    HttpResponse::Ok().body("Hello, World!")
 }
 
-#[tokio::main]
-async fn main() {
-    let make_svc = make_service_fn(|_conn| {
-        let service = service_fn(handle_request);
-        async { Ok::<_, hyper::Error>(service) }
-    });
-
-    let addr = ([127, 0, 0, 1], 3000).into();
-    let server = Server::bind(&addr).serve(make_svc);
-
-    if let Err(e) = server.await {
-        eprintln!("Server error: {}", e);
-    }
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(web::resource("/hello-rust/").to(index))
+    })
+    .bind("127.0.0.1:3000")?
+    .run()
+    .await
 }
