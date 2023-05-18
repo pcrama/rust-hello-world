db41f4f1f9f061d0614d4d6be6a40a7020765226
Philippe Crama <pcfeb0009@gmx.com>
2023-05-18 10:33:38 +0200

    Extract testable functions into library to access them from cargo test

---
+++ src/lib.rs
@@ -0,0 +1,45 @@
+use actix_files::NamedFile;
+use actix_web::{get, web, App, HttpRequest, HttpResponse};
+use std::path::PathBuf;
+use tera::Tera;
+
+#[get("/")]
+pub async fn index() -> HttpResponse {
+    HttpResponse::Ok().body("Hello, World!")
+}
+
+#[get("/{user_id}/{name}")]
+pub async fn greet_user_id_and_name(path: web::Path<(u32, String)>, tera: web::Data<Tera>) -> HttpResponse {
+    let (user_id, name) = path.into_inner();
+    let mut context = tera::Context::new();
+    context.insert("name", &name);
+    context.insert("user_id", &user_id.to_string());
+    let rendered = tera.render("greet_user_id_and_name.html", &context).unwrap();
+    HttpResponse::Ok().body(rendered)
+}
+
+#[get("/assets/{filename}")]
+pub async fn static_files(req: HttpRequest) -> actix_web::Result<NamedFile> {
+    let path: PathBuf = ["assets", req.match_info().query("filename")].iter().collect();
+    Ok(NamedFile::open(path)?)
+}
+
+// Type signature would have been impossible without
+// https://github.com/actix/actix-web/issues/1147#issuecomment-1509937750.  See
+// also its discussion of `configure' and
+// https://github.com/actix/actix-web/issues/1402
+pub fn create_app() -> App<
+	impl actix_web::dev::ServiceFactory<
+		actix_web::dev::ServiceRequest,
+		Config = (),
+		Response = actix_web::dev::ServiceResponse<impl actix_web::body::MessageBody>,
+		Error = actix_web::Error,
+		InitError = (),
+	>> {
+    App::new()
+        .app_data(web::Data::new(Tera::new("templates/**/*").unwrap()))
+        .service(web::scope("/hello-rust")
+            .service(static_files)
+            .service(greet_user_id_and_name)
+            .service(index))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,9 @@
-use actix_files::NamedFile;
-use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer};
-use std::path::PathBuf;
-use tera::Tera;
-
-#[get("/")]
-async fn index() -> HttpResponse {
-    HttpResponse::Ok().body("Hello, World!")
-}
-
-/// extract path info from "/{user_id}/{name}" url
-/// {user_id} - deserializes to a u32
-/// {name} - deserializes to a String
-#[get("/{user_id}/{name}")]
-async fn greet_user_id_and_name(path: web::Path<(u32, String)>) -> HttpResponse {
-    let (user_id, name) = path.into_inner();
-    let tera = Tera::new("templates/**/*").unwrap();
-    let mut context = tera::Context::new();
-    context.insert("name", &name);
-    context.insert("user_id", &user_id.to_string());
-    let rendered = tera.render("greet_user_id_and_name.html", &context).unwrap();
-    HttpResponse::Ok().body(rendered)
-}
-
-#[get("/assets/{filename}")]
-async fn static_files(req: HttpRequest) -> actix_web::Result<NamedFile> {
-    let path: PathBuf = ["assets", req.match_info().query("filename")].iter().collect();
-    Ok(NamedFile::open(path)?)
-}
+use hello_world_lib::create_app;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().service(web::scope("/hello-rust")
-			       .service(static_files)
-	                       .service(greet_user_id_and_name)
-			       .service(index))
-    })
-    .bind("127.0.0.1:3000")?
-    .run()
-    .await
+    actix_web::HttpServer::new(|| create_app())
+        .bind("127.0.0.1:3000")?
+        .run()
+        .await
 }
