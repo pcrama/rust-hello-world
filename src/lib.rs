616adf815704d1a2aaf4ab3b7b357789549d9f78
Philippe Crama <pcfeb0009@gmx.com>
2023-05-20 22:48:29 +0200

    Receive form data

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,22 @@
 use actix_files::NamedFile;
-use actix_web::{get, web, App, HttpRequest, HttpResponse};
+use actix_web::{get, post, web, App, HttpRequest, HttpResponse};
+use serde::Deserialize;
 use std::path::PathBuf;
 use tera::Tera;
+
+#[derive(Deserialize)]
+#[allow(non_snake_case)]
+pub struct MeterReadings {
+    timestamp: String,
+    pv_2022_prod_kWh: u32,
+    pv_2012_prod_kWh: u32,
+    peak_hour_consumption_kWh: u32,
+    off_hour_consumption_kWh: u32,
+    peak_hour_injection_kWh: u32,
+    off_hour_injection_kWh: u32,
+    gas_m3: u32,
+    water_m3: u32,
+}
 
 #[get("/")]
 pub async fn index() -> HttpResponse {
@@ -42,6 +57,25 @@ pub async fn get_meter_readings_form(tera: web::Data<Tera>) -> HttpResponse {
     HttpResponse::Ok().body(rendered)
 }
 
+#[post("/meter-readings")]
+pub async fn submit_meter_readings(form: web::Form<MeterReadings>) -> HttpResponse {
+    // Perform the desired operations with the submitted data here
+    log::info!(
+        "Received data for {}: pv_2022_prod_kWh={}, pv_2012_prod_kWh={}, peak_hour_consumption_kWh={}, off_hour_consumption_kWh={}, peak_hour_injection_kWh={}, off_hour_injection_kWh={}, gas_m3={}, water_m3={}",
+        form.timestamp,
+	form.pv_2022_prod_kWh,
+	form.pv_2012_prod_kWh,
+	form.peak_hour_consumption_kWh,
+	form.off_hour_consumption_kWh,
+	form.peak_hour_injection_kWh,
+	form.off_hour_injection_kWh,
+	form.gas_m3,
+	form.water_m3,);
+
+    // Return a response indicating success or failure
+    HttpResponse::Ok().body("Form submitted successfully")
+}
+
 // Type signature would have been impossible without
 // https://github.com/actix/actix-web/issues/1147#issuecomment-1509937750.  See
 // also its discussion of `configure' and
@@ -61,6 +95,7 @@ pub fn create_app() -> App<
             web::scope("/hello-rust")
                 .service(static_files)
                 .service(get_meter_readings_form)
+                .service(submit_meter_readings)
                 .service(greet_user_id_and_name)
                 .service(index),
         )
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use hello_world_lib::create_app;
 
+fn configure_logging() {
+    env_logger::Builder::from_env(env_logger::Env::default())
+        .format_timestamp(None)
+        .init();
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    configure_logging();
+    log::info!("Starting HttpServer...");
     actix_web::HttpServer::new(|| create_app())
         .bind("127.0.0.1:3000")?
         .run()
