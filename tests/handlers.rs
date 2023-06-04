2ffc7eede0b3306d2443edb83d2ad7081b0400cf
Philippe Crama <pcfeb0009@gmx.com>
2023-06-04 14:59:07 +0200

    Add test for get_meter_readings_form

--- tests/handlers.rs
+++ tests/handlers.rs
@@ -1,6 +1,5 @@
 use actix_web::{http::StatusCode, test};
-
-use hello_world_lib::create_app;
+use hello_world_lib::{create_app, MeterReadingsUserInput};
 
 #[actix_rt::test]
 async fn test_greet_user_id_and_name() {
@@ -63,4 +62,42 @@ async fn test_get_meter_readings_form() {
             "Response does not contain expected HTML: input",
         );
     }
+}
+
+#[actix_rt::test]
+async fn test_submit_meter_readings() {
+    let mut app = test::init_service(create_app()).await;
+
+    // Create a mock form input
+    let form_input = MeterReadingsUserInput {
+        timestamp: "2023-05-22 20:40".to_string(),
+        pv_2022_prod_kWh: Some("1848.2".to_string()),
+        pv_2012_prod_kWh: None,
+        peak_hour_consumption_kWh: Some(" 10.5  ".to_string()),
+        off_hour_consumption_kWh: Some(" ".to_string()),
+        peak_hour_injection_kWh: None,
+        off_hour_injection_kWh: None,
+        gas_m3: Some("  5,6".to_string()),
+        water_m3: Some("6,5  ".to_string()),
+    };
+
+    // Encode the form input into the body content
+    let form_body = form_input.to_string();
+
+    // Create a test request and send it to the server
+    let req = test::TestRequest::post()
+        .uri("/hello-rust/meter-readings")
+        .set_form(form_body)
+        .to_request();
+    let resp = test::call_service(&mut app, req).await;
+
+    // Assert that the response is successful
+    assert!(resp.status().is_success());
+
+    // Read the response body
+    let body = test::read_body(resp).await;
+
+    // Assert that the response body contains the expected message
+    let expected_msg = "Form submitted successfully: Received data for 2023-05-22 20:40: pv_2022_prod_kWh=1848.2, pv_2012_prod_kWh=-99.9, peak_hour_consumption_kWh=10.5, off_hour_consumption_kWh=-99.9, peak_hour_injection_kWh=-99.9, off_hour_injection_kWh=-99.9, gas_m3=5.6, water_m3=6.5";
+    assert_eq!(body, expected_msg.as_bytes());
 }
