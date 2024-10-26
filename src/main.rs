25b2462f705a67f841d69c1b7082ca2b82770a06
Philippe Crama <pcfeb0009@gmx.com>
2024-10-26 08:38:44 +0000

    Add unit tests

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,50 @@ fn parse_kwh(line: &str, prefix: &str) -> Result<Option<f64>, ParseFloatError> {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn strip_prefix_and_suffix_prefix_mismatch_expect_none() {
+        assert_eq!(
+            strip_prefix_and_suffix("1-0:2.8.2(002457.202*kWh)", "mismatch", "*kWh)"),
+            None
+        )
+    }
+
+    #[test]
+    fn strip_prefix_and_suffix_suffix_mismatch_expect_none() {
+        assert_eq!(
+            strip_prefix_and_suffix("1-0:2.8.2(002457.202*kWh)", "1-0:2.8.2(", "mismatch)"),
+            None
+        )
+    }
+
+    #[test]
+    fn strip_prefix_and_suffix_match_expect_sub_string() {
+        assert_eq!(
+            strip_prefix_and_suffix("1-0:2.8.2(002457.202*kWh)", "1-0:2.8.2(", "*kWh)"),
+            Some("002457.202")
+        )
+    }
+
+    #[test]
+    fn parse_kwh_expect_float() {
+        assert_eq!(parse_kwh("prefix(12.34*kWh)", "prefix("), Ok(Some(12.34)))
+    }
+
+    #[test]
+    fn parse_kwh_mismatch_expect_none() {
+        assert_eq!(parse_kwh("prefix(12.34*kWh)", "mismtach"), Ok(None))
+    }
+
+    #[test]
+    fn parse_kwh_bad_float_format_expect_err() {
+        assert!(parse_kwh("prefix(bad-float*kWh)", "prefix(").is_err())
+    }
+}
+
 fn main() {
     println!("Hello, world!");
     match strip_prefix_and_suffix("1-0:2.8.2(002457.202*kWh)", "1-0:1.8.2(", "*kWh)") {
